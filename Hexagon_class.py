475c1a391175e51474359402790e7e12bd46b540
Shlomo <sh.z.wind@gmail.com>
2025-06-25 11:38:39 +0300

    create the class Hexagon

---
+++ Hexagon_class.py
@@ -0,0 +1,8 @@
+from Shape_class import Shape
+import math
+
+class Hexagon(Shape):
+    def __init__(self, side):
+        self.side = side
+    def get_area(self):
+        return (self.side ** 2) * ((math.sqrt(3) * 3) / 2)
