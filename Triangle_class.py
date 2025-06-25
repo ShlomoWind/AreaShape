18af32e6a5b3c62cd6ef823d33f4d8bdc2651dd8
Shlomo <sh.z.wind@gmail.com>
2025-06-25 12:11:56 +0300

    added function get_perimeter on class Triangle

--- Triangle_class.py
+++ Triangle_class.py
@@ -1,8 +1,13 @@
 from Rectangle_class import Rectangle
 
+# Right-angled triangle only:
 class Triangle(Rectangle):
     def __init__(self, base,height):
         super().__init__(base,height)
 
     def get_area(self):
         return super().get_area() /2
+
+    def get_perimeter(self):
+        pythagoras = (self.width ** 2 + self.height ** 2) ** 0.5
+        return self.height + self.width + pythagoras
